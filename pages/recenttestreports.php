4a5b60b7c862c81b96e440102eb8cd00f99b1931
DevMorde <andreiocampo87@gmail.com>
2025-03-18 20:24:10 +0800

    testresult

---
+++ pages/recenttestreports.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Reports</title>
+    <link rel="stylesheet" href="../assets/css/patientSidebar.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="mainContainer">
+        <div class="panel" id="sideBarPanel">
+            <div class="header">
+                <span class="logo">
+                    <img src="../assets/images/Medisync LogoMark.png" alt="Medisync">
+                </span> 
+                <span class="logoText">
+                    <span class="firstName">First Name</span>
+                    <span class="lastName">Last Name</span>
+                </span>
+            </div>
+            <div class="navigation">
+                <button type="button" id="dashboardButton">
+                    <i class='bx bxs-dashboard'></i>
+                    <p>Dashboard</p>
+                </button>
+                <button type="button" id="appointmentsButton">
+                    <i class='bx bxs-calendar'></i>
+                    <p>Appointments</p>
+                </button>
+                <button type="button" id="prescriptionsButton">
+                    <i class='bx bxs-capsule'></i>
+                    <p>Prescriptions</p>
+                </button>
+                <button type="button" id="billsButton">
+                    <i class='bx bxs-wallet' ></i>
+                    <p>Bills</p>
+                </button>
+                <button type="button" id="testReportsButton">
+                    <i class='bx bxs-notepad'></i>
+                    <p>Test Reports</p>
+                </button>
+            </div>
+            <div class="logout" id="accountButton">
+                <button type="button">
+                    <i class='bx bx-log-out' ></i>
+                    <p>Log Out</p>
+                </button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
