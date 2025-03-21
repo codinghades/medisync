23b8519e301c4a6b5a5e85644875c4d68394cd6c
Dreeows <matienzoandrews@gmail.com>
2025-03-21 16:58:02 +0800

    login fixed

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,3 +1,17 @@
 function loadPage(page) {
     window.location.href = page + ".php";
 }
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("accountButton").addEventListener("click", function () {
+        fetch("../process/logout.php", { method: "POST" })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                window.location.href = "../index.php";
+            } else {
+                alert("Logout failed. Try again.");
+            }
+        })
+        .catch(() => alert("An error occurred. Please try again."));
+    });
+});
