0432b8a7abdf4ce63b53e6cd7ebaf0a67675bebe
Dreeows <matienzoandrews@gmail.com>
2025-03-21 22:40:16 +0800

    prescriptions done

--- pages/prescriptions.php
+++ pages/prescriptions.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prescriptions</title>
     <link rel="stylesheet" href="../assets/css/patientPrescriptions.css">
+    <script src="../assets/js/prescription.js"></script>
 </head>
 <body>
     <div class="mainContainer">
