2e22d58ea450c358d69d53a2253d181f9240fa46
Ryell Villanueva <villanuevaryell@gmail.com>
2025-03-23 22:57:19 +0800

    # payment sa Admin po hehe

---
+++ admin/payment.php
@@ -0,0 +1,86 @@
+<?php
+    include '../includes/adminSidebar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payments</title>
+    <link rel="stylesheet" href="../assets/css/paymentAdmin.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+<div class="mainContainer">
+    <div class="header-container">
+        <h2>Patient's Name: </h2>
+        <input type="text" class="search-bar" placeholder="Search...">
+    </div>
+        <div class="Container">
+                    <div class="box">
+                    <div class="box1">Patient: <b>Name</b></div>
+                    <div class="box2">Total  Charges
+                    <div class="content">$$$,$$$</div>
+                    </div>
+                    <div class="box2">Status
+                    <div class="content">-statement-</div>
+                    </div>
+                    </div>
+        </div>
+        <div class="Container">
+                    <div class="box">
+                    <div class="box1">Patient: <b>Name</b></div>
+                    <div class="box2">Total  Charges
+                    <div class="content">$$$,$$$</div>
+                    </div>
+                    <div class="box2">Status
+                    <div class="content">-statement-</div>
+                    </div>
+                    </div>
+        </div>
+        <div class="Container">
+                    <div class="box">
+                    <div class="box1">Patient: <b>Name</b></div>
+                    <div class="box2">Total  Charges
+                    <div class="content">$$$,$$$</div>
+                    </div>
+                    <div class="box2">Status
+                    <div class="content">-statement-</div>
+                    </div>
+                    </div>
+        </div>
+        <div class="Container">
+                    <div class="box">
+                    <div class="box1">Patient: <b>Name</b></div>
+                    <div class="box2">Total  Charges
+                    <div class="content">$$$,$$$</div>
+                    </div>
+                    <div class="box2">Status
+                    <div class="content">-statement-</div>
+                    </div>
+                    </div>
+        </div>
+        <div class="Container">
+                    <div class="box">
+                    <div class="box1">Patient: <b>Name</b></div>
+                    <div class="box2">Total  Charges
+                    <div class="content">$$$,$$$</div>
+                    </div>
+                    <div class="box2">Status
+                    <div class="content">-statement-</div>
+                    </div>
+                    </div>
+        </div>
+        <div class="Container">
+                    <div class="box">
+                    <div class="box1">Patient: <b>Name</b></div>
+                    <div class="box2">Total  Charges
+                    <div class="content">$$$,$$$</div>
+                    </div>
+                    <div class="box2">Status
+                    <div class="content">-statement-</div>
+                    </div>
+                    </div>
+        </div>
+</div>    
+</body>
